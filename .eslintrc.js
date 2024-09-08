ff8738716047f6fac0529c10d27d4f8f8c26afb9
chicuongb2107180 <cuongb2107180@student.ctu.edu.vn>
2024-09-08 16:24:30 +0700

    Cau hinh Eslint cho du an

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+//https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+};
