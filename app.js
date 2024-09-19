f21585553de32a37ab7a5dc666dc6185eaa7a859
chicuongb2107180 <cuongb2107180@student.ctu.edu.vn>
2024-09-19 13:55:25 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require('./app/api_error');
 const app = express();
 const contactsRouter = require('./app/routes/contact_route');
 app.use(cors());
@@ -8,4 +9,10 @@ app.get('/', (req, res) => {
         res.json({ message: 'Well come to contact book application' });
 });
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+        return next(new AppiError(404, 'Not found'));
+});
+app.use((err, req, res, next) => {
+        return res.status(err.statuscode || 500).json({ message: err.message || 'Internal server error' });
+});
 module.exports = app;
